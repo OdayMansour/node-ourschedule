71f8177c6d20a18b933b3db2c1f26efe7eebe4b7
Oday Mansour <odaym@nsour.net>
2018-06-30 17:07:20 +0200

    Cleaned up old commented out code

--- server/index.js
+++ server/index.js
@@ -6,13 +6,6 @@ var db = new sqlite3.Database('schedule.db');
 var app = express();
 
 db.serialize(checkSchema)
-//   db.run("CREATE TABLE lorem (info TEXT)");
-
-//   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-//   for (var i = 0; i < 10; i++) {
-//       stmt.run("Ipsum " + i);
-//   }
-//   stmt.finalize();
 
 function checkSchema() {
     db.run("SELECT 1 FROM schedule", function(err, row) {
